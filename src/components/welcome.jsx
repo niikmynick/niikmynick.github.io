71d0708688d33cc1a8d29384148154db80ec75aa
Nikita <fk724s@mail.ru>
2024-06-11 13:31:13 +0300

    Updates

--- src/components/welcome.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const WelcomeBlock = () => {
-    return (
-        <div id="welcome-content">
-            <h1>welcome to niik’s world</h1>
-        </div>
-    )
-}
